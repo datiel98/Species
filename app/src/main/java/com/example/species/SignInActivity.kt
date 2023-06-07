b11e5276f8ed22cf541b81375a22f58e20f1f1b0
Tuan Dat <tuandat2402n@outlook.com>
2023-06-07 23:54:13 +0700

    [Password Dialog]

--- app/src/main/java/com/example/species/SignInActivity.kt
+++ app/src/main/java/com/example/species/SignInActivity.kt
@@ -1,27 +1,62 @@
 package com.example.species
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.species.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         firebaseAuth = FirebaseAuth.getInstance()
         binding.textView.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.forgotPassTextView.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            val dialogLayout = inflater.inflate(R.layout.forgot_pass_dialog, null)
+            val editText = dialogLayout.findViewById<EditText>(R.id.email_text)
+
+            with(builder) {
+                setTitle("Reset Password")
+                setPositiveButton("Reset"){dialog, which ->
+                    val emailReset = editText.text.toString()
+
+                    Firebase.auth.sendPasswordResetEmail(emailReset)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(this@SignInActivity, "Email Sent", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                }
+                setNegativeButton("Cancel"){dialog, which ->
+                    Toast.makeText(this@SignInActivity, "Canceled", Toast.LENGTH_SHORT).show()
+
+                }
+                setView(dialogLayout)
+                show()
+            }
         }
 
         binding.button.setOnClickListener {
@@ -46,6 +81,9 @@ class SignInActivity : AppCompatActivity() {
         }
     }
 
+    private fun showDialog() {
+
+    }
     override fun onStart() {
         super.onStart()
 
